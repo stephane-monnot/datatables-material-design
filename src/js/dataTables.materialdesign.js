01a37eeb33a323e40435150c5d30c38b1560867a
Monnot Stéphane <monnot.stephane@gmail.com>
2015-10-13 12:45:34 +0200

    Add border and redesign datatable footer info and paginate

--- src/js/dataTables.materialdesign.js
+++ src/js/dataTables.materialdesign.js
@@ -21,7 +21,7 @@
             dom:
             "<'row'<'col-sm-6'l><'col-sm-6'f>>" +
             "<'row'<'col-sm-12'tr>>" +
-            "<'row'<'col-sm-5'i><'col-sm-7'p>>",
+            "<'row row-footer'<'col-sm-5'i><'col-sm-7'p>>",
             renderer: 'bootstrap'
         });
 
